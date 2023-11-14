7c72f17658db213018f912d379d59ac83f385ec7
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2023-11-14 15:56:09 +0000

    push

--- app/pfc/page.tsx
+++ app/pfc/page.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { title } from "@/components/primitives";
 import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
+import { InputNumber, Button, message, Table, Spin, Result, Tooltip, Radio, Tag, Popover, Avatar, Space } from 'antd';
 
 export default function PricingPage() {
 
@@ -20,8 +21,30 @@ export default function PricingPage() {
 				</div>
 			</CardHeader>
 			<Divider/>
-			<CardBody>
-				<p>Make beautiful websites regardless of your design experience.</p>
+			<CardBody style={{textAlign: 'center'}}>
+				<Radio.Group
+					style={{ marginBottom: 16, width: '100%' }}
+				>
+					<Radio.Button value={1}>Rock <Avatar src="https://symbl-world.akamaized.net/i/webp/f2/0e29d778af528ff18585b3c4088835.webp" size={18}/></Radio.Button>
+					<Radio.Button value={2}>Paper <Avatar src="https://symbl-world.akamaized.net/i/webp/77/ec9b6d839eb7a9868c98b397842442.webp" size={18}/></Radio.Button>
+					<Radio.Button value={3}>Scissors <Avatar src="https://symbl-world.akamaized.net/i/webp/c4/aa8b2a5d6d7304241d56de9f82e3d9.webp" size={18}/></Radio.Button>
+				</Radio.Group>
+				<InputNumber
+						placeholder="Amount"
+						inputMode="numeric"
+						style={{  }}
+						addonBefore={
+						<img src="https://nanswap.com/logo/XNO.svg" width='25px' style={{}}/>
+						}
+						min={0}
+						addonAfter={
+						<Space>
+							<Button size='small' style={{}}>÷2</Button>
+							<Button size='small' style={{}}>×2</Button>
+							<Button size='small' style={{}}>Max</Button>
+						</Space>
+						}
+					/>
 			</CardBody>
 			<Divider/>
 			<CardFooter>
